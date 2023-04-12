25b79746eb189d5c92b6514c15d1abf4e5dc7450
voynow <voynow99@gmail.com>
2023-04-11 22:02:41 -0400

    Unit tests command - needs a better model

--- README.md
+++ README.md
@@ -2,27 +2,30 @@
 
 # turbo_docs
 
-A command line tool to quickly generate formatted & user-friendly README.md files for small & large repositories.
+turbo_docs is a speech-to-text generator for Python 3.6 - 3.9. This package enables users to generate a formatted and user-friendly README.md file, generte code summaries, and create unit tests using command-line options. 
 
 ## Requirements
+The following libraries must be installed: 
+- setuptools
+- wheel 
+- twine 
+- requests 
+- openai 
+- click 
+- pyperclip 
 
-* `setuptools`
-* `wheel`
-* `twine`
-* `requests`
-* `openai`
-* `click`
-* `pyperclip`
+## Usage 
+The package contains the following command-line options for running specified processes: 
+- --copy (true/false): Copy directory text to clipboard
+- --create_readme (true/false): Generate readme file
+- --create_readme_plus (true/false): Generate formatted and user-friendly readme file
+- --create_tests (true/false): Generate unit tests 
 
-## Running
 
-Install the requirements with `pip install -r requirements.txt` and then run `python setup.py install`.
+The script `generate.driver` serves as the entry point for the package. It uses the OpenAI GPT-3 API to send a user prompt and receive completions with up to 2048 tokens. 
 
-The main driver function for `turbo_docs` is invoked with the `turbo_docs` command from the terminal.
+A `.gitignore` file is used to create a list of ignored files, including README.md, tests, and any file in the current directory that starts with a period. The `directory.py` script retrieves the content of unmasked files and stores them in a dictionary. 
 
-## Usage
+## Development Status
 
-`turbo_docs` takes the following options and generate README.md files accordingly:
-* `--copy`: Copy the directory text to clipboard
-* `--create_readme`: Generate README.md file
-* `--create_readme_plus`: Generate readme for larger repositories
+alpha
--- turbo_docs/generate.py
+++ turbo_docs/generate.py
@@ -1,11 +1,12 @@
 import click
 import os
+from pathlib import Path
 import pyperclip
 from turbo_docs.utils import directory, openai_api, cli_options
 
 
 def run_create_readme(text):
-    """ Generates a README.md
+    """ Generate a formatted and user-friendly README.md file using the provided text.
     """
     prompt = f"Create a formatted & user-friendly readme.md from the following:\n\n{text}"
     response = openai_api.gpt_completion_wrapper(prompt)
@@ -15,25 +16,53 @@ def run_create_readme(text):
 
 
 def run_create_readme_plus(files):
-    """ Extends the run_create_readme function to handle larger repos
+    """ Generate a README.md file with summarized code content from the provided files.
     """
     responses = {}
     for file_path, file_content in files.items():
         if os.stat(file_path).st_size and file_path.split(".")[1]:
-            print(f"(--create_readme_plus) Summarizing from {file_path}")
+            print(f"(--create_readme_plus) Summarizing {file_path}")
             prompt = f"Summarize the following code, especially maintaining key logic:\n{file_content}"
             responses[file_path] = openai_api.gpt_completion_wrapper(prompt)
+
     psuedocode = "\n\n\n".join([f"{file_path}:\n{summary}" for file_path, summary in responses.items()])
     run_create_readme(psuedocode)
     print(f"(--create_readme_plus) Generated README.md")
+
+
+def run_create_tests(test_file_path, file_content):
+    """ Generate unit tests for the provided code file content and save them in a test file.
+    """
+    print(f"(--create_tests) Generating unit tests for {test_file_path}")
+    prompt = f"Generate unit tests for the following code:\n{file_content}"
+    test_content = openai_api.gpt_completion_wrapper(prompt)
+    with open(test_file_path, "w") as test_file:
+        test_file.write(test_content)
+
+
+def run_create_tests_helper(files):
+    """ Create unit tests for the provided code files and save them in a 'tests' directory.
+    """
+    tests_dir = "tests"
+    Path(tests_dir).mkdir(exist_ok=True)
+
+    for file_path, file_content in files.items():
+        if os.stat(file_path).st_size and file_path.split(".")[1]:
+            test_file_name = f"test_{os.path.basename(file_path)}"
+            test_file_path = os.path.join(tests_dir, test_file_name)
+    
+            resp = input(f"Would you like to create unit tests for {test_file_name} (Y/n):")
+            if resp != "n" and resp != "N":
+                run_create_tests(test_file_path, file_content)
 
 
 @click.command()
 @cli_options.copy
 @cli_options.create_readme
 @cli_options.create_readme_plus
-def driver(copy: bool, create_readme: bool, create_readme_plus: bool) -> None:
-    """ Main driver function that generates a README.md file for the current repository.
+@cli_options.create_tests
+def driver(copy: bool, create_readme: bool, create_readme_plus: bool, create_tests: bool) -> None:
+    """ The main driver function that handles various command-line options for generating documentation and tests.
     """
     files = directory.get_files()
     dir_text = "\n\n".join([f"{name}:\n\n{content}" for name, content in files.items()])
@@ -50,6 +79,10 @@ def driver(copy: bool, create_readme: bool, create_readme_plus: bool) -> None:
     # Generate docs.md file if specified
     if create_readme_plus:
         run_create_readme_plus(files)
+
+    # Generate unit tests for each code file if specified
+    if create_tests:
+        run_create_tests_helper(files)
 
 
 if __name__ == '__main__':
