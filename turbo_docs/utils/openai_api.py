765a13171ea27728e346679aed57a22b38032ce2
voynow <voynow99@gmail.com>
2023-04-14 10:48:02 -0400

    Only ask for opanai creds if necessary

--- turbo_docs/utils/openai_api.py
+++ turbo_docs/utils/openai_api.py
@@ -1,16 +1,23 @@
 import json
 import os
-import openai
 
-openai.api_key = os.environ.get('OPENAI_API_KEY')
-if not openai.api_key:
-    raise ValueError("Cannot find API key. Run the following command: export OPENAI_API_KEY=<your_api_key>")
+
+def openai_init():
+    """
+    """
+    import openai
+    openai.api_key = os.environ.get('OPENAI_API_KEY')
+
+    if not openai.api_key:
+        raise ValueError("Cannot find API key. Run the following command: export OPENAI_API_KEY=<your_api_key>")
+    return openai
 
 
 def gpt_completion_wrapper(prompt):
     """ Send a prompt to the OpenAI GPT-3 API and receive a completion.
     """
-    completions = openai.Completion.create(
+    openai_package = openai_init()
+    completions = openai_package.Completion.create(
         engine="text-davinci-003",
         prompt=prompt,
         max_tokens=2048,
