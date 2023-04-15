356cafebf96d261e2506f128494ed392e34609c8
voynow <voynow99@gmail.com>
2023-04-15 12:31:07 -0400

    Fix spacing from spaces to tabs

--- turbo_docs/utils/directory.py
+++ turbo_docs/utils/directory.py
@@ -4,48 +4,48 @@ from pathlib import Path
 from typing import List, Dict
 
 def ignored_files_init() -> List[str]:
-    """ Initialize a list of ignored files and including README.md, docs.md, etc.
-    """
-    ignored_files = ["README.md", "tests"]
-    for file in os.listdir():
-        if file[0] == ".":
-            ignored_files.append(file)
-    return ignored_files
+	""" Initialize a list of ignored files and including README.md, docs.md, etc.
+	"""
+	ignored_files = ["README.md", "tests"]
+	for file in os.listdir():
+		if file[0] == ".":
+			ignored_files.append(file)
+	return ignored_files
 
 def read_gitignore() -> List[str]:
-    """ Read .gitignore file and return the list of ignored files.
-    """
-    ignore_files = ignored_files_init()
-    try:
-        with open(".gitignore", "r") as gitignore:
-            for line in gitignore:
-                ignore_files.append(line.strip())
-    except FileNotFoundError:
-        raise ValueError(".gitignore file required for excluding files from documentation generation")
-    return ignore_files
+	""" Read .gitignore file and return the list of ignored files.
+	"""
+	ignore_files = ignored_files_init()
+	try:
+		with open(".gitignore", "r") as gitignore:
+			for line in gitignore:
+				ignore_files.append(line.strip())
+	except FileNotFoundError:
+		raise ValueError(".gitignore file required for excluding files from documentation generation")
+	return ignore_files
 
 def ignore_filepath(filepath: str, ignore_files: List[str]) -> bool:
-    """ Check if a filepath should be ignored based on the ignore_files list.
-    """
-    for part in Path(filepath).parts:
-        if part in ignore_files:
-            return True
-    return False
+	""" Check if a filepath should be ignored based on the ignore_files list.
+	"""
+	for part in Path(filepath).parts:
+		if part in ignore_files:
+			return True
+	return False
 
 def get_files() -> Dict:
-    """ Retrieve the content of all files in the current directory, excluding those listed in the ignore_files.
-    """
-    files_dict = {}
-    ignore_files = read_gitignore()
+	""" Retrieve the content of all files in the current directory, excluding those listed in the ignore_files.
+	"""
+	files_dict = {}
+	ignore_files = read_gitignore()
 
-    # Iterate over files
-    for root, _, files in os.walk("."):
-        for file in files:
-            filepath = os.path.join(root, file).replace(".\\", "")
+	# Iterate over files
+	for root, _, files in os.walk("."):
+		for file in files:
+			filepath = os.path.join(root, file).replace(".\\", "")
 
-            # If not in ignore, collect file text
-            if not ignore_filepath(filepath, ignore_files):
-                with open(filepath, "r") as f:
-                    content = f.read().replace(" " * 4, "\t")
-                files_dict[filepath] = content
-    return files_dict
+			# If not in ignore, collect file text
+			if not ignore_filepath(filepath, ignore_files):
+				with open(filepath, "r") as f:
+					content = f.read().replace(" " * 4, "\t")
+				files_dict[filepath] = content
+	return files_dict
--- turbo_docs/utils/openai_api.py
+++ turbo_docs/utils/openai_api.py
@@ -3,26 +3,26 @@ import os
 
 
 def openai_init():
-    """
-    """
-    import openai
-    openai.api_key = os.environ.get('OPENAI_API_KEY')
+	"""
+	"""
+	import openai
+	openai.api_key = os.environ.get('OPENAI_API_KEY')
 
-    if not openai.api_key:
-        raise ValueError("Cannot find API key. Run the following command: export OPENAI_API_KEY=<your_api_key>")
-    return openai
+	if not openai.api_key:
+		raise ValueError("Cannot find API key. Run the following command: export OPENAI_API_KEY=<your_api_key>")
+	return openai
 
 
 def gpt_completion_wrapper(prompt):
-    """ Send a prompt to the OpenAI GPT-3 API and receive a completion.
-    """
-    openai_package = openai_init()
-    completions = openai_package.Completion.create(
-        engine="text-davinci-003",
-        prompt=prompt,
-        max_tokens=2048,
-        n=1,
-        stop=None,
-    )
-    return completions.choices[0]['text']
+	""" Send a prompt to the OpenAI GPT-3 API and receive a completion.
+	"""
+	openai_package = openai_init()
+	completions = openai_package.Completion.create(
+		engine="text-davinci-003",
+		prompt=prompt,
+		max_tokens=2048,
+		n=1,
+		stop=None,
+	)
+	return completions.choices[0]['text']
 
