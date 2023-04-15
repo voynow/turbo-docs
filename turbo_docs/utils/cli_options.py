6ec2005e5d3a8de14733df7a3ed6a74f9f13d8c5
voynow <voynow99@gmail.com>
2023-04-15 12:31:27 -0400

    Updates for create_docstring command

--- turbo_docs/utils/cli_options.py
+++ turbo_docs/utils/cli_options.py
@@ -2,33 +2,33 @@ import click
 from typing import Callable
 
 def copy(func: Callable) -> Callable:
-    return click.option(
-        '--copy',
-        default=False,
-        is_flag=True,
-        help='Copy the directory text to clipboard'
-    )(func)
+	return click.option(
+		'--copy',
+		default=False,
+		is_flag=True,
+		help='Copy the directory text to clipboard'
+	)(func)
 
 def create_readme(func: Callable) -> Callable:
-    return click.option(
-        '--create_readme',
-        default=False,
-        is_flag=True,
-        help='Generate README.md file'
-    )(func)
+	return click.option(
+		'--create_readme',
+		default=False,
+		is_flag=True,
+		help='Generate README.md file'
+	)(func)
 
 def create_readme_plus(func: Callable) -> Callable:
-    return click.option(
-        '--create_readme_plus',
-        default=False,
-        is_flag=True,
-        help='Generate readme for larger repositories'
-    )(func)
+	return click.option(
+		'--create_readme_plus',
+		default=False,
+		is_flag=True,
+		help='Generate readme for larger repositories'
+	)(func)
 
 def create_tests(func: Callable) -> Callable:
-    return click.option(
-        '--create_tests',
-        default=False,
-        is_flag=True,
-        help='Generate unit tests for each code file'
-    )(func)
+	return click.option(
+		'--create_tests',
+		default=False,
+		is_flag=True,
+		help='Generate unit tests for each code file'
+	)(func)
