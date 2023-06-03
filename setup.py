e9756119a29e704ab768f44463e1ca393880618b
voynow <voynow99@gmail.com>
2023-06-03 15:01:02 -0400

    Adding toml to setup install

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
 	name="turbo_docs",
-	version="0.8.4",
+	version="0.8.5",
 	packages=find_packages(),
 	install_requires=[
 		"requests",
@@ -11,6 +11,7 @@ setup(
 		"pyperclip",
 		"redbaron",
 		"gitpython",
+    	"toml",
 	],
 	entry_points={
 		"console_scripts": [
