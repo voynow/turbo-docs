5513385b40d6f116a1ae73a10ba2a5f93949ca97
voynow <voynow99@gmail.com>
2023-12-01 09:06:32 -0500

    Finalizing template & test generation

--- turbo_docs/commands/readme.py
+++ turbo_docs/commands/readme.py
@@ -2,13 +2,8 @@ TEMPLATE = """
 "You are an expert software developement assistant. Write a README.md for the following repo:
 {repo}
 
-Here are some recommendations for the README.md:
-- written in highly formatted/structured markdown
-- badeges for github stars and pypi if applicable
-- contains a detailed outline written assuming the user knows nothing about the repo
-- a section on why use this repo, pitching the repo to the user
-- repo structure formatted as a tree
-- code block example usage of important functionality, especially functionality exposed to the user
+- Imagine this repo were trending on GitHub. What sort of information is required in a readme of this caliber?
+- Write from the perspective of the user and from a contributer. Curate a best in class user experience by providing ample detail, context, and examples.
 - emoji are encouraged. One for each section at minimum.
 """
 
@@ -21,7 +16,8 @@ def readme(repo, template=TEMPLATE):
     from turbo_docs.utils import openai_api
 
     readme = "README.md"
-    response = openai_api.gpt_completion(repo)
+    prompt = TEMPLATE.format(repo=repo)
+    response = openai_api.gpt_completion(prompt)
 
     with open(readme, "w", encoding="utf-8") as readme_file:
         readme_file.write(response)
