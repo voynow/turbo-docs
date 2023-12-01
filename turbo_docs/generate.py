71c1d6f111add8e4b317566bcc79677edf398692
voynow <voynow99@gmail.com>
2023-12-01 08:27:16 -0500

    Removing docs command

--- turbo_docs/generate.py
+++ turbo_docs/generate.py
@@ -3,8 +3,8 @@ from pathlib import Path
 import pyperclip
 import tiktoken
 from turbo_docs.commands import readme as readme_module
-from turbo_docs.commands import docs as docs_module
 from turbo_docs.utils import directory, cli_options
+
 
 def resolve_model(gpt3):
     if gpt3:
@@ -19,23 +19,23 @@ def resolve_model(gpt3):
     print(f"Using model: {model}")
     return model
 
+
 def num_tokens_from_string(string: str, encoding_name: str = "cl100k_base") -> int:
     """Returns the number of tokens in a text string."""
     encoding = tiktoken.get_encoding(encoding_name)
     num_tokens = len(encoding.encode(string, allowed_special="all"))
     return num_tokens
 
+
 @click.command()
 @cli_options.copy
 @cli_options.readme
 @cli_options.gpt3
-@cli_options.docs
 @cli_options.narrative
 def driver(
     copy: bool,
     readme: bool,
     gpt3: bool,
-    docs: bool,
     narrative: str,
 ) -> None:
     """
@@ -49,9 +49,6 @@ def driver(
 
     'turbo_docs --gpt3' uses GPT-3.5-turbo-16k
         Useful if you don't have GPT-4 access
-
-    'turbo_docs --docs' generates a docs for each file
-        Useful for keeping documentation up to date
     """
     dir_text_dict = directory.get_repo_text_dict()
 
@@ -62,11 +59,6 @@ def driver(
         model = resolve_model(gpt3)
         readme_module.readme(dir_text_str, model, narrative=narrative)
         print("Generated README.md")
-
-    if docs:
-        model = resolve_model(gpt3)
-        docs_module.docs(dir_text_dict, model)
-        print("Generated documentation")
 
     if copy:
         dir_text_str = directory.convert_dict_to_string(dir_text_dict)
--- turbo_docs/utils/cli_options.py
+++ turbo_docs/utils/cli_options.py
@@ -1,42 +1,32 @@
 import click
 from typing import Callable
 
+
 def copy(func: Callable) -> Callable:
     return click.option(
-        '--copy',
+        "--copy",
         default=False,
         is_flag=True,
-        help='Copy the directory text to clipboard'
+        help="Copy the directory text to clipboard",
     )(func)
+
 
 def readme(func: Callable) -> Callable:
     return click.option(
-        '--readme',
-        default=False,
-        is_flag=True,
-        help='Generate README.md file'
+        "--readme", default=False, is_flag=True, help="Generate README.md file"
     )(func)
+
 
 def narrative(func: Callable) -> Callable:
     return click.option(
-        '--narrative',
+        "--narrative",
         default="",
         type=str,
-        help='Provide a narrative to guide the tone and content of the README'
+        help="Provide a narrative to guide the tone and content of the README",
     )(func)
+
 
 def gpt3(func: Callable) -> Callable:
     return click.option(
-        '--gpt3',
-        default=False,
-        is_flag=True,
-        help='Use the GPT-3.5 Turbo model'
-    )(func)
-
-def docs(func: Callable) -> Callable:
-    return click.option(
-        '--docs',
-        default=False,
-        is_flag=True,
-        help='Generate documentation for all code files'
+        "--gpt3", default=False, is_flag=True, help="Use the GPT-3.5 Turbo model"
     )(func)
